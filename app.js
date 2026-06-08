9f97fc934e148379f8d98f2d3d2b9ed338f9a4dd
wolfez <wolfderechter@gmail.com>
2026-06-08 14:31:58 +0200

    weekly and monthly cron jobs for notification processing

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 import { app } from "mu";
 import bodyParser from "body-parser";
+import { CronJob } from "cron";
+import { processNotifications } from "./lib/tasks";
+import { WEEKLY_CRON, MONTHLY_CRON } from "./env";
+
+const weeklyJob = new CronJob(WEEKLY_CRON, () =>
+  processNotifications("weekly"),
+);
+weeklyJob.start();
+console.log(
+  `Registered a weekly task for fetching and processing instance subscription notifications at ${new Date().toISOString()}`,
+);
+
+const monthlyJob = new CronJob(MONTHLY_CRON, () =>
+  processNotifications("monthly"),
+);
+monthlyJob.start();
+console.log(
+  `Registered a monthly task for fetching and processing instance subscription notifications at ${new Date().toISOString()}`,
+);
 
 app.use(
   bodyParser.json({
