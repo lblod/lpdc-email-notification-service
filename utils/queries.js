40ad0c8f9656435a67c740191a7f7f3885c0edd4
kikef98 <kike.franssen@soprasteria.com>
2026-07-07 16:47:23 +0200

    mistake with capitals

--- utils/queries.js
+++ utils/queries.js
@@ -364,23 +364,23 @@ export async function getReviewStatusChanges(instanceUris, since, orgUuid) {
   });
 }
 
-export async function updateLastNotifiedAt(notificationpreferencesUri, date) {
+export async function updateLastNotifiedAt(notificationPreferencesUri, date) {
   const queryString = `
     ${PREFIXES}
     DELETE {
       GRAPH ?g {
-        ${sparqlEscapeUri(notificationpreferencesUri)} lpdcExt:lastNotifiedAt ?oldTime .
+        ${sparqlEscapeUri(notificationPreferencesUri)} lpdcExt:lastNotifiedAt ?oldTime .
       }
     }
     INSERT {
       GRAPH ?g {
-        ${sparqlEscapeUri(notificationpreferencesUri)} lpdcExt:lastNotifiedAt ${sparqlEscapeDateTime(date)} .
+        ${sparqlEscapeUri(notificationPreferencesUri)} lpdcExt:lastNotifiedAt ${sparqlEscapeDateTime(date)} .
       }
     }
     WHERE {
       GRAPH ?g {
-        ${sparqlEscapeUri(notificationpreferencesUri)} a lpdcExt:NotificationPreference .
-        OPTIONAL { ${sparqlEscapeUri(notificationpreferencesUri)} lpdcExt:lastNotifiedAt ?oldTime . }
+        ${sparqlEscapeUri(notificationPreferencesUri)} a lpdcExt:NotificationPreference .
+        OPTIONAL { ${sparqlEscapeUri(notificationPreferencesUri)} lpdcExt:lastNotifiedAt ?oldTime . }
       }
       FILTER STRSTARTS(STR(?g), "http://mu.semte.ch/graphs/organizations/")
       FILTER STRENDS(STR(?g), "/LoketLB-LPDCGebruiker")
@@ -391,10 +391,10 @@ export async function updateLastNotifiedAt(notificationpreferencesUri, date) {
 
 /**
  * Puts email in the right mail folder graph for sending
- * @param {object} notificationpreferences
+ * @param {object} notificationPreferences
  * @param {Object} email
  */
-export async function insertEmail(notificationpreferences, email) {
+export async function insertEmail(notificationPreferences, email) {
   try {
     // Temporary debug log
     const now = new Date();
@@ -411,7 +411,7 @@ export async function insertEmail(notificationpreferences, email) {
                                       nmo:emailTo ${sparqlEscapeString(email.to)} ;
                                       nmo:messageFrom ${sparqlEscapeString(FROM_EMAIL_ADDRESS)} ;
                                       dct:creator ${sparqlEscapeUri(SERVICE_URI)} ;
-                                      dct:references ${sparqlEscapeUri(notificationpreferences.uri)} ;
+                                      dct:references ${sparqlEscapeUri(notificationPreferences.uri)} ;
                                       dct:created ${sparqlEscapeDateTime(now)} .
       }
     }`;
