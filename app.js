962692d7a742323359fe8b166cbc7f280e61bf53
wolfez <wolfderechter@gmail.com>
2026-07-17 13:49:24 +0200

    use correct timezone for cron

--- app.js
+++ app.js
@@ -5,18 +5,18 @@ import { processNotifications } from "./lib/tasks";
 import { CRON_FREQUENCY } from "./env";
 
 // Daily cron to process the notifications for both frequencies, in case of downtime/missed runs the maximum delay would be a day instead of a week/month
-new CronJob(
-  CRON_FREQUENCY,
-  async () => {
+CronJob.from({
+  cronTime: CRON_FREQUENCY,
+  onTick: async () => {
     try {
       await processNotifications();
     } catch (error) {
       console.error("Failed to process notifications", error);
     }
   },
-  null,
-  true,
-);
+  start: true,
+  timeZone: "Europe/Brussels",
+});
 
 app.use(
   bodyParser.json({
